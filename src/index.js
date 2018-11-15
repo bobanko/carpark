b03b04dfea1144256303390ada1ef5d014e9be41
bobanko <mishko.vladimir@gmail.com>
2018-11-15 20:17:27 +0200

    road +garage + some other stuff

--- src/index.js
+++ src/index.js
@@ -1,64 +1,78 @@
 //import * as THREE from "./three";
 import { THREE } from "/three.js";
 
-import { Car } from "./components/car/index.js";
+import { Car } from "./components/car.js";
+import { Road } from "./components/road.js";
+import { Garage } from "./components/garage.js";
 
 var scene = new THREE.Scene();
+
 var camera = new THREE.PerspectiveCamera(
-  70, //45, // 75,
+  45, // 75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 5;
+camera.position.z = 7;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.BasicShadowMap;
+
 document.body.appendChild(renderer.domElement);
 
 let car = new Car();
-let obj = scene.add(car.group);
+scene.add(car.group);
+//todo: get rid of groups?
+let road = new Road({ length: 10 });
+scene.add(road.group);
 
-console.log(car.group.children);
-let acceleration = 0;
+let garage = new Garage();
+scene.add(garage.group);
+
+scene.add(new THREE.AmbientLight(0xff0000, 1.2));
+
+scene.fog = new THREE.Fog(new THREE.Color(0x000000), 0.0025, 100);
+
+const updatableComponents = [car];
+const props = {
+  acceleration: 0
+};
 
 function animate() {
   requestAnimationFrame(animate);
 
-  //carSprite.position.x += 0.01;
-  car.update({ acceleration });
+  updatableComponents.forEach(component => {
+    component.update(props);
+  });
 
-  // car.group.children.forEach(wheel => {
-  //   // wheel.rotation.x += Math.PI / 20;
-  //   wheel.rotation.x += Math.PI / 20;
-  //   wheel.rotation.y += Math.PI / 20;
-  //   wheel.rotation.z += Math.PI / 20;
-  // });
+  let { x, y } = car.group.position;
 
-  //car.group.rotation.z -= Math.PI * 0.005;
+  camera.position.x = x;
+  camera.position.y = y;
 
   renderer.render(scene, camera);
 }
 animate();
 
+//todo: somehow wrap eventlisteners?
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown({ code }) {
   console.log(code);
 
   switch (code) {
     case "ArrowLeft":
-      acceleration = -1;
+      props.acceleration = -1;
       break;
     case "ArrowRight":
-      acceleration = 1;
+      props.acceleration = 1;
       break;
     case "Space":
       car.speed = 0;
       car.group.position.x = 0;
       break;
   }
-
-  console.log(acceleration);
 }
 
 document.addEventListener("keyup", onDocumentKeyUp, false);
@@ -69,9 +83,36 @@ function onDocumentKeyUp({ code }) {
     case "ArrowLeft":
     case "ArrowRight":
     case "Space":
-      acceleration = 0;
+      props.acceleration = 0;
       break;
   }
-
-  console.log(acceleration);
 }
+//debug tools
+
+window.scene = scene; //debug
+document.addEventListener("wheel", ({ wheelDeltaY }) => {
+  camera.translateZ(Math.sign(wheelDeltaY));
+  console.log(camera.position.z);
+});
+
+//todo: make camera follow lerp
+
+function initCamera() {
+  let controls = new THREE.TrackballControls(camera);
+
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+
+  controls.noZoom = false;
+  controls.noPan = false;
+
+  controls.staticMoving = true;
+  controls.dynamicDampingFactor = 0.3;
+
+  controls.keys = [65, 83, 68];
+
+  updatableComponents.push(controls);
+}
+
+//initCamera();
