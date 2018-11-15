cf33aef8e1718425ba098ff7fb7080c8b964724d
bobanko <mishko.vladimir@gmail.com>
2018-11-15 22:06:22 +0200

    road trees made planes

--- src/components/road.js
+++ src/components/road.js
@@ -1,5 +1,5 @@
 import { THREE } from "/three.js";
-import { loadSprite } from "/helpers.js";
+import { createImagePlane, createImageBox } from "/helpers.js";
 
 const IMAGES = {
   BLACK: "./images/square-black.png",
@@ -14,22 +14,18 @@ export class Road {
     this.group = new THREE.Group();
 
     for (let i = 0; i < length; i++) {
-      const spriteMap = new THREE.TextureLoader().load(IMAGES.WHITE);
-
-      let geometry = new THREE.BoxGeometry(1, 1, 1);
-      let material = new THREE.MeshBasicMaterial({
-        map: spriteMap,
-        color: 0xffffff
+      let roadPart = createImageBox({
+        href: IMAGES.WHITE,
+        position: [i, 0, 0]
       });
-      let roadPart = new THREE.Mesh(geometry, material);
-      roadPart.position.set(i, 0, 0);
-      //cube.rotation.x = Math.PI / 4;
+
       this.group.add(roadPart);
 
-      let tree = loadSprite(IMAGES.TREE);
-
       let treeZ = i % 2 ? 1 : -1;
-      tree.position.set(i, 1, treeZ / 2);
+      let tree = createImagePlane({
+        href: IMAGES.TREE,
+        position: [i, 1, treeZ / 2]
+      });
 
       this.group.add(tree);
     } ///for
