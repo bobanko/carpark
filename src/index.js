94adc232d782a784e0ef17cd5b5cfa9e4b7a69b5
bobanko <mishko.vladimir@gmail.com>
2018-11-16 02:06:27 +0200

    controls added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Car } from "./components/car.js";
 import { Road } from "./components/road.js";
 import { Garage } from "./components/garage.js";
 
+const messageDelay = 200;
+
 var scene = new THREE.Scene();
 scene.background = new THREE.Color("#c2d4d1");
 
@@ -19,26 +21,47 @@ camera.position.z = 7;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.BasicShadowMap;
+//renderer.shadowMap.type = THREE.BasicShadowMap;
+renderer.shadowMapType = THREE.PCFSoftShadowMap;
 
 document.body.appendChild(renderer.domElement);
 
 const messageFail = document.querySelector(".message-fail");
 const messageWin = document.querySelector(".message-win");
 
-messageFail.hidden = true;
-messageWin.hidden = true;
-
 let car = new Car();
+car.group.position.x = 10;
 scene.add(car.group);
 //todo: get rid of groups?
 let road = new Road({ length: 10 });
 scene.add(road.group);
 
 let garage = new Garage();
+garage.group.position.set(10, -1, 0);
 scene.add(garage.group);
+//ambient light
+let ambient = new THREE.AmbientLight(0xffffff, 0.9);
+window.ambient = ambient;
+scene.add(ambient);
+//directional light
 
-scene.add(new THREE.AmbientLight(0xff0000, 1.2));
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+window.directionalLight = directionalLight;
+//scene.add(directionalLight);
+
+{
+  let light = new THREE.DirectionalLight(0xffffff, 1, 100);
+  light.position.set(0, 1, 0); //default; light shining from top
+  light.castShadow = true; // default false
+  scene.add(light);
+
+  //Set up shadow properties for the light
+  light.shadow.mapSize.width = 512; // default
+  light.shadow.mapSize.height = 512; // default
+  light.shadow.camera.near = 0.5; // default
+  light.shadow.camera.far = 500; // default
+  window.direct = light;
+}
 
 scene.fog = new THREE.Fog(new THREE.Color(0x000000), 0.0025, 100);
 
@@ -54,6 +77,7 @@ function update() {
 
   let { x, y } = car.group.position;
 
+  //todo: extract as part of camera component
   camera.position.x = x;
   camera.position.y = y;
 
@@ -92,11 +116,9 @@ function update() {
   // );
 
   if (!carCrashed && isCarParked && isStopped) {
-    carPark();
+    // carPark();
   }
 }
-
-let messageDelay = 1000;
 
 let carParked = false;
 function carPark() {
@@ -161,6 +183,45 @@ function onDocumentKeyUp({ code }) {
       break;
   }
 }
+
+document.querySelector(".button-restart").addEventListener("click", () => {
+  window.location.reload();
+});
+
+//left
+document.querySelector(".button-left").addEventListener("mousedown", () => {
+  onDocumentKeyDown({ code: "ArrowLeft" });
+});
+
+document.querySelector(".button-left").addEventListener("touchstart", () => {
+  onDocumentKeyDown({ code: "ArrowLeft" });
+});
+
+document.querySelector(".button-left").addEventListener("mouseup", () => {
+  onDocumentKeyUp({ code: "ArrowLeft" });
+});
+
+document.querySelector(".button-left").addEventListener("touchend", () => {
+  onDocumentKeyUp({ code: "ArrowLeft" });
+});
+
+//right;
+document.querySelector(".button-right").addEventListener("mousedown", () => {
+  onDocumentKeyDown({ code: "ArrowRight" });
+});
+
+document.querySelector(".button-right").addEventListener("touchstart", () => {
+  onDocumentKeyDown({ code: "ArrowRight" });
+});
+
+document.querySelector(".button-right").addEventListener("mouseup", () => {
+  onDocumentKeyUp({ code: "ArrowRight" });
+});
+
+document.querySelector(".button-right").addEventListener("touchend", () => {
+  onDocumentKeyUp({ code: "ArrowRight" });
+});
+
 //debug tools
 
 window.scene = scene; //debug
